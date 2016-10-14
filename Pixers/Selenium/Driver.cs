8f1aa02836774b214a675102b6858ecbf5592d2a
drk182 <182temporary@gmail.com>
2016-10-14 14:33:50 +0200

    Stworzenie projektu
    Utworzenie drivera (FF)

---
+++ Pixers/Selenium/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Pixers.Selenium
+{
+    public class Driver
+    {
+        public static FirefoxDriver Instance { get; set; }
+
+        public static void Initialize()
+        {
+            Instance = new FirefoxDriver();
+        }
+
+        public static void Close()
+        {
+            Instance.Quit();
+        }
+    }
+}
