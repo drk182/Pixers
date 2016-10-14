f14aeb3131e27b25b3145438baefdf76175c8166
drk182 <182temporary@gmail.com>
2016-10-14 14:38:50 +0200

    Dodanie konfiguracji do Core i NUnita do projektu z testami

---
+++ Pixers.Core/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixers.Core
+{
+    public class Configuration
+    {
+        private const string LoginKey = "Login";
+        private const string PasswordKey = "Password";
+        private const string BaseAddressKey = "BaseAddress";
+
+        public static string GetUserLogin
+        {
+            get { return ConfigurationManager.AppSettings[LoginKey]; }
+        }
+
+        public static string GetUserPassword
+        {
+            get { return ConfigurationManager.AppSettings[PasswordKey]; }
+        }
+
+        public static string GetBaseAddress
+        {
+            get { return ConfigurationManager.AppSettings[BaseAddressKey]; }
+        }
+
+    }
+}
